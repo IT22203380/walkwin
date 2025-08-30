20b15cc1b4bc1e5056aeae3c7de1b35d84c49b4d
SenuriDis <senudissanayake18@gmail.com>
2025-08-30 14:50:27 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,20 @@
 
+const mongoose = require('mongoose');
+
+async function connectToDatabase(mongoUri) {
+	if (!mongoUri) {
+		throw new Error('MONGODB_URI is not defined');
+	}
+
+	mongoose.set('strictQuery', true);
+
+	await mongoose.connect(mongoUri, {
+		serverSelectionTimeoutMS: 15000,
+	});
+
+	return mongoose.connection;
+}
+
+module.exports = { connectToDatabase };
+
+
